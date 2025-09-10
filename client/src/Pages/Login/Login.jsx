d72336e7e8dd3cd2b1b3875b66ab0008a850fdf0
gokul <gokulsunil2015@gmail.com>
2025-09-10 16:38:47 +0530

    vishva's jayan chitta

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import styles from "./Login.module.css";
+import { FaDumbbell } from "react-icons/fa";
+import { GiShuttlecock } from "react-icons/gi";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className={styles.container}>
+      {/* Floating icons */}
+      <div className={styles.icons}>
+        <GiShuttlecock className={styles.icon} style={{ top: "10%", left: "5%" }} />
+        <FaDumbbell className={styles.icon} style={{ top: "15%", left: "20%" }} />
+        <GiShuttlecock className={styles.icon} style={{ top: "30%", left: "10%" }} />
+        <FaDumbbell className={styles.icon} style={{ top: "40%", right: "20%" }} />
+        <GiShuttlecock className={styles.icon} style={{ bottom: "20%", left: "15%" }} />
+        <FaDumbbell className={styles.icon} style={{ bottom: "10%", right: "10%" }} />
+        <GiShuttlecock className={styles.icon} style={{ bottom: "5%", left: "5%" }} />
+        <FaDumbbell className={styles.icon} style={{ top: "60%", right: "5%" }} />
+      </div>
+
+      {/* Center box */}
+      <div className={styles.card}>
+        <div className={styles.logo}>🏓 Courtly</div>
+        <p className={styles.subtitle}>Please select your role to proceed</p>
+        <h2 className={styles.title}>Welcome to Courtly</h2>
+
+        <button className={styles.roleBtn}>Admin</button>
+        <button className={styles.roleBtn}>Reception</button>
+
+        <div className={styles.actionBtns}>
+          <button className={styles.signupBtn}>Sign Up</button>
+          <button className={styles.loginBtn}>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
