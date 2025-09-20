5bb2fcd495f09d416da727a063c331608f682285
Adithyan <adithyan.vjmd@gmail.com>
2025-09-20 09:49:09 +0530

    completed:reception

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,63 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './Pages/Login/Login.jsx';
-import AdminLogin from './Pages/AdminLogin/AdminLogin.jsx';
-import TrainerLogin from './Pages/Trainer/TrainerLogin.jsx';
-import SidebarSwitching from './Pages/SidebarSwitching/SidebarSwitching.jsx'; // Import the component
-import ReceptionLogin from './Pages/ReceptionLogin/ReceptionLogin.jsx';
-import ReceptionDashboard from './Pages/ReceptionDashboard/ReceptionDashboard.jsx'
-import { useState } from 'react';
-import BookingManagement from './Pages/BookingPage/BookingPage.jsx';
-import ReportPage from './Pages/ReportPage/ReportPage.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login/Login.jsx";
+import AdminLogin from "./Pages/AdminLogin/AdminLogin.jsx";
+import TrainerLogin from "./Pages/Trainer/TrainerLogin.jsx";
+import SidebarSwitching from "./Pages/SidebarSwitching/SidebarSwitching.jsx"; // Import the component
+import ReceptionLogin from "./Pages/ReceptionLogin/ReceptionLogin.jsx";
+import ReceptionDashboard from "./Pages/ReceptionDashboard/ReceptionDashboard.jsx";
+import { useState } from "react";
+import BookingManagement from "./Pages/BookingPage/BookingPage.jsx";
+import ReportPage from "./Pages/ReportPage/ReportPage.jsx";
 
 // Trainer
-import TrainerSidebarSwitching from './Pages/TrainerSidebarSwitching/TrainerSidebarSwitching.jsx';
+import TrainerSidebarSwitching from "./Pages/TrainerSidebarSwitching/TrainerSidebarSwitching.jsx";
+
+// Reception
+import ReceptionistSidebarSwitching from "./Pages/ReceptionistSidebarSwitching/ReceptionistSidebarSwitching.jsx";
 
 function App() {
-  const [activeNav, setActiveNav] = useState('Dashboard');
+  const [activeNav, setActiveNav] = useState("Dashboard");
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/Adminlogin' element={<AdminLogin />} />
-          <Route path='/Trainerlogin' element={<TrainerLogin />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/Adminlogin" element={<AdminLogin />} />
+          <Route path="/Trainerlogin" element={<TrainerLogin />} />
 
-          <Route path='/Admindashboard' element={
-            <SidebarSwitching
-              activeNav={activeNav}
-              setActiveNav={setActiveNav}
-            />
-          } />
-          <Route path='/booking' element={<BookingManagement />} />
-          <Route path='/ReceptionLogin' element={<ReceptionLogin />} />
-          <Route path='/Receptiondashboard' element={<ReceptionDashboard />} />
-          <Route path='/report' element={<ReportPage />} />
+          <Route
+            path="/Admindashboard"
+            element={
+              <SidebarSwitching
+                activeNav={activeNav}
+                setActiveNav={setActiveNav}
+              />
+            }
+          />
+          <Route
+            path="/Receptionistdashboard"
+            element={
+              <ReceptionistSidebarSwitching
+                activeNav={activeNav}
+                setActiveNav={setActiveNav}
+              />
+            }
+          />
+          <Route path="/booking" element={<BookingManagement />} />
+          <Route path="/ReceptionLogin" element={<ReceptionLogin />} />
+          <Route path="/report" element={<ReportPage />} />
 
           {/* Trainer */}
-          <Route path='/Trainerdashboard' element={
-            <TrainerSidebarSwitching
-              activeNav={activeNav}
-              setActiveNav={setActiveNav}
-            />
-          } />
+          <Route
+            path="/Trainerdashboard"
+            element={
+              <TrainerSidebarSwitching
+                activeNav={activeNav}
+                setActiveNav={setActiveNav}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
