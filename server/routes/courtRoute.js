ba04bdebe6129f7861bb7fa00f798bd1ef619be7
gokul <gokulsunil2015@gmail.com>
2025-09-20 15:09:24 +0530

    court data

--- server/routes/courtRoute.js
+++ server/routes/courtRoute.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { createCourt, getCourt, editCourt, deleteCourt } from '../controller/courtController.js'
+import { createCourt, getCourt, editCourt, deleteCourt, getCourtStatistics } from '../controller/courtController.js'
 
 const courtRouter = express.Router()
 courtRouter.route("/createCourt").post(createCourt)
 courtRouter.route("/fetchCourts").get(getCourt)
 courtRouter.route("/editCourt/:id").put(editCourt)
 courtRouter.route("/deleteCourt/:id").delete(deleteCourt)
+courtRouter.route("/full-statistics").get(getCourtStatistics);
+
+
 
 
 export default courtRouter
